9c6c744cad0d466c4920e76fbe40dfcb7804a229
YooInKeun <keun0390@naver.com>
2020-06-29 22:56:48 +0900

    feat: add posts service

---
+++ yik-springboot-webservice/src/main/java/com/yooinkeun/webservice/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.yooinkeun.webservice.springboot.service.posts;
+
+import com.yooinkeun.webservice.springboot.domain.posts.PostsRepository;
+import com.yooinkeun.webservice.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
