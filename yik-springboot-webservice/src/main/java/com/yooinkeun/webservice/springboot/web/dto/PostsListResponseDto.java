8d658c5b7f496a7140c4dd5f15546e95bb19f5ee
YooInKeun <keun0390@naver.com>
2020-07-16 07:55:54 +0900

    feat: add posts list response dto

---
+++ yik-springboot-webservice/src/main/java/com/yooinkeun/webservice/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,20 @@
+package com.yooinkeun.webservice.springboot.web.dto;
+
+import com.yooinkeun.webservice.springboot.domain.posts.Posts;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
