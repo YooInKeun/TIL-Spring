e0660888052689c18aa344a26a8463b39b22c3a7
YooInKeun <keun0390@naver.com>
2020-07-17 22:18:02 +0900

    feat: add find all posts service method

--- yik-springboot-webservice/src/main/java/com/yooinkeun/webservice/springboot/service/posts/PostsService.java
+++ yik-springboot-webservice/src/main/java/com/yooinkeun/webservice/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.yooinkeun.webservice.springboot.service.posts;
 
 import com.yooinkeun.webservice.springboot.domain.posts.Posts;
 import com.yooinkeun.webservice.springboot.domain.posts.PostsRepository;
+import com.yooinkeun.webservice.springboot.web.dto.PostsListResponseDto;
 import com.yooinkeun.webservice.springboot.web.dto.PostsResponseDto;
 import com.yooinkeun.webservice.springboot.web.dto.PostsSaveRequestDto;
 import com.yooinkeun.webservice.springboot.web.dto.PostsUpdateRequestDto;
@@ -11,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +36,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
