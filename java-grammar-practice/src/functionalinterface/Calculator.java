68a165d0ee1ac0e3190f8e5bccf15f3cd011fdcc
유인근 <keun0390@naver.com>
2021-05-08 22:00:06 +0900

    chore: 함수형 인터페이스 사용 예제 추가

---
+++ java-grammar-practice/src/functionalinterface/Calculator.java
@@ -0,0 +1,6 @@
+package functionalinterface;
+
+@FunctionalInterface
+public interface Calculator {
+    int calculate(int num1, int num2);
+}
--- java-grammar-practice/src/functionalinterface/Main.java
+++ java-grammar-practice/src/functionalinterface/Main.java
@@ -1,4 +1,19 @@
 package functionalinterface;
 
 public class Main {
+
+    static class CalculatorService {
+        public int calculate(Calculator calculator, int num1, int num2) {
+            return calculator.calculate(num1, num2);
+        }
+    }
+
+    public static void main(String[] args) {
+        CalculatorService calculatorService = new CalculatorService();
+        Calculator addition = (num1, num2) -> num1 + num2;
+        System.out.println(calculatorService.calculate(addition, 3, 6));
+
+        Calculator multiply = (num1, num2) -> num1 * num2;
+        System.out.println(calculatorService.calculate(multiply, 3, 6));
+    }
 }
