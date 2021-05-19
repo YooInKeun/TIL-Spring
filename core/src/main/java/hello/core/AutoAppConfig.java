886a132d948a07aa90c3998757c3378cbaa80f2e
유인근 <keun0390@naver.com>
2021-05-20 08:00:51 +0900

    test: 컴포넌트 스캔으로 빈 등록 및 테스트 코드 추가

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // AppConfig 와의 충돌 막기
+)
+public class AutoAppConfig {
+}
