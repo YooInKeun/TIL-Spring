22bb3d31b4c99106b8e15368a90e64d1823fc3ab
유인근 <keun0390@naver.com>
2021-06-14 00:04:42 +0900

    test: request scope bean, proxyMode를 사용한 예시 코드 추가

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,7 +2,6 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,18 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
-    private final LogDemeService logDemeService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
 
     @GetMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestUrl = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestUrl(requestUrl);
 
         myLogger.log("controller test");
-        logDemeService.logic("testId");
+        logDemoService.logic("testId");
         return "ok";
     }
 }
