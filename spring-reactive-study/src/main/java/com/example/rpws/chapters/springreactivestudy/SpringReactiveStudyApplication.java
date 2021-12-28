86aadcb48319f98f6dd4f80010e0c27f47b4f2b5
YooInKeun <keun0390@naver.com>
2021-12-28 23:55:58 +0900

    chore: add spring-reactive-study project

---
+++ spring-reactive-study/src/main/java/com/example/rpws/chapters/springreactivestudy/SpringReactiveStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpws.chapters.springreactivestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveStudyApplication.class, args);
+    }
+
+}
---
+++ spring-reactive-study/src/test/java/com/example/rpws/chapters/springreactivestudy/SpringReactiveStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rpws.chapters.springreactivestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
