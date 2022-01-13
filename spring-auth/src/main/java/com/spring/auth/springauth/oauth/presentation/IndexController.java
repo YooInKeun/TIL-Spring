c13e6545bdfe7cf29d60aad53339fdbebec81420
YooInKeun <keun0390@naver.com>
2022-01-14 00:39:45 +0900

    feat: add user login

---
+++ spring-auth/src/main/java/com/spring/auth/springauth/oauth/presentation/IndexController.java
@@ -0,0 +1,26 @@
+package com.spring.auth.springauth.oauth.presentation;
+
+import com.spring.auth.springauth.oauth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "index";
+    }
+}
