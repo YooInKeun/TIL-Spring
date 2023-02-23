291b7690c90a7d92672a23f3f9499ecab385e94b
YooInKeun <keun0390@naver.com>
2023-02-24 02:10:50 +0900

    chore: change package structure

--- publisher/src/main/kotlin/rabbitmq/PublisherController.kt
+++ publisher/src/main/kotlin/rabbitmq/PublisherController.kt
@@ -1,4 +1,4 @@
-package com.example.publisher
+package rabbitmq
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.web.bind.annotation.GetMapping
--- publisher/src/main/kotlin/rabbitmq/RabbitMQConfig.kt
+++ publisher/src/main/kotlin/rabbitmq/RabbitMQConfig.kt
@@ -1,4 +1,4 @@
-package com.example.publisher
+package rabbitmq
 
 import org.springframework.amqp.core.Binding
 import org.springframework.amqp.core.BindingBuilder
