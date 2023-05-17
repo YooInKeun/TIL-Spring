cf4ec8b606592a6145e27393cd87b4ac21b7a09b
YooInKeun <keun0390@naver.com>
2023-05-17 22:52:04 +0900

    chore: chatgpt api test

--- chat-gpt/src/main/kotlin/Main.kt
+++ chat-gpt/src/main/kotlin/Main.kt
@@ -6,14 +6,24 @@ fun main() {
     val apiKey = "" // OpenAI API Key
     val prompt = "Hello, how are you?"
 
-    val url = URL("https://api.openai.com/v1/engines/davinci-codex/completions")
+    val url = URL("https://api.openai.com/v1/chat/completions")
     val con = url.openConnection() as HttpURLConnection
     con.requestMethod = "POST"
     con.setRequestProperty("Content-Type", "application/json")
     con.setRequestProperty("Authorization", "Bearer $apiKey")
 
     // API 요청 바디 생성
-    val requestBody = "{\"prompt\": \"$prompt\", \"max_tokens\": 60}"
+    val requestBody = """
+            {
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                        {
+                            "role": "user", 
+                            "content": "$prompt"
+                        }
+                ],
+                "temperature": 0.7
+            }"""
 
     // API 요청 바디 전송
     con.doOutput = true
