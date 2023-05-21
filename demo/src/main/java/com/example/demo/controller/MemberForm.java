8d9175c771af2d3edd692796d15f86b9d828bed9
YooInKeun <keun0390@naver.com>
2023-05-21 20:43:53 +0900

    feat: add member post, get

---
+++ demo/src/main/java/com/example/demo/controller/MemberForm.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+public class MemberForm {
+
+    private String name;
+
+    public MemberForm(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
